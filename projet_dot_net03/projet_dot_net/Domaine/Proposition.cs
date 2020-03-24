a1f8b034ff1806cf38a110a0181cdcf3caf16097
wiwi5959 <wiwi5959@outlook.fr>
2020-03-24 19:41:05 +0100

    wip

---
+++ projet_dot_net03/projet_dot_net/Domaine/Proposition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projet_dot_net.Domaine
+{
+    public partial class Proposition
+    {
+        public int Id { get; set; }
+        public TimeSpan? Duree { get; set; }
+        public string Sujet { get; set; }
+        public int EntrepriseId { get; set; }
+        public int StageId { get; set; }
+
+        public virtual Entreprise Entreprise { get; set; }
+        public virtual ConventionStage Stage { get; set; }
+    }
+}
