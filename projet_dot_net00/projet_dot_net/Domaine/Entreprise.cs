40086ccb388df542953fb15630074d1fd4065212
wiwi5959 <wiwi5959@outlook.fr>
2020-03-24 19:36:34 +0100

    wip

---
+++ projet_dot_net00/projet_dot_net/Domaine/Entreprise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projet_dot_net.Domaine
+{
+    public partial class Entreprise
+    {
+        public Entreprise()
+        {
+            ConventionStage = new HashSet<ConventionStage>();
+            Proposition = new HashSet<Proposition>();
+            Rdv = new HashSet<Rdv>();
+        }
+
+        public int Id { get; set; }
+        public string Nom { get; set; }
+
+        public virtual ICollection<ConventionStage> ConventionStage { get; set; }
+        public virtual ICollection<Proposition> Proposition { get; set; }
+        public virtual ICollection<Rdv> Rdv { get; set; }
+    }
+}
